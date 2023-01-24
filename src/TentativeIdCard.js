3af5fa5586d1889be2154d0725d3b0976312b744
j4jeetu18 <j4jeetu18@gmail.com>
2023-01-24 18:21:47 +0530

    functionality of generating results implemented

--- src/TentativeIdCard.js
+++ src/TentativeIdCard.js
@@ -1,19 +1,20 @@
-export default function TentativeIdCard(props){
-    console.log(props.entity)
-    return(
-    <>
-        <div id="tentative-id-card">
-            <img id="tentative-header" src={`${props.entity.uploadedHeader}`}></img>
-            <div id="tentative-details-container">
-                <img id="tentative-uploaded-photo" src={`${props.entity.uploadedPhoto}`}></img>
-                <div id="tentative details">
-                    Name:{props.entity.name}<br></br>
-                    Roll NO:{props.entity.rollNo}<br></br>
-                    Class:{props.entity.class}<br></br>
-                    Father's Name:{props.entity.fathersName}<br></br>
+import altImage from "./resources/altImage.png"
+import altHeader from "./resources/altHeader.jpg"
+
+export default function TentativeIdCard(props) {
+    console.log(props.entity.uploadedPhoto);
+    return (
+        <>
+                <img id="tentative-header" src={`${props.entity.uploadedHeader===undefined?altHeader:props.entity.uploadedHeader}`}/>
+                <div id="tentative-details-container">
+                    <img id="tentative-uploaded-photo" src={`${props.entity.uploadedPhoto===undefined?altImage:props.entity.uploadedPhoto}`}/>
+                    <div id="tentative-details">
+                        <h3>{props.entity.name===""?<i>"Name here"</i>:props.entity.name}</h3>
+                        Roll NO:&emsp;{props.entity.rollNo}<br></br>
+                        Class:&emsp;{props.entity.class}<br></br>
+                        Father:&nbsp;{(props.entity.fathersName)}<br></br>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </>
+        </>
     )
 }
